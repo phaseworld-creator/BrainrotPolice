97e04b321b77ea87e8f738c3c4d30e8ba2eb163f
Phaseworld <279915747+phaseworld-creator@users.noreply.github.com>
2026-07-08 21:41:29 +0200

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -18,7 +18,7 @@ function env.import(id)
 end
 
 function env.getgitpath(where)
-    local mainBuild = "https://raw.githubusercontent.com/IcantAffordSynapse/BrainrotPolice/refs/heads/main/"
+    local mainBuild = "https://raw.githubusercontent.com/phaseworld-creator/BrainrotPolice/refs/heads/dev/"
     if where == "src" then
         return mainBuild .. "src/"
     elseif where == "games" then
@@ -38,7 +38,7 @@ env.GitHubToolsList = {}
 
 local function fetchToolsList()
     local httpservice = game:GetService("HttpService")
-    local apiURL = "https://api.github.com/repos/IcantAffordSynapse/BrainrotPolice/contents/src/Tools"
+    local apiURL = "https://api.github.com/repos/phaseworld-creator/BrainrotPolice/contents/src/Tools?ref=dev"
     
     local success, response = pcall(function()
         return game:HttpGet(apiURL)
@@ -74,5 +74,5 @@ game:GetService("GuiService"):SetGameplayPausedNotificationEnabled(false)
 loadstring(game:HttpGet(getgitpath("src").."ui.lua"))()
 
 if queue_on_teleport then
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/IcantAffordSynapse/BrainrotPolice/refs/heads/main/src/init.lua"))()')
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/phaseworld-creator/BrainrotPolice/refs/heads/dev/src/init.lua"))()')
 end
